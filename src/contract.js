16b1bb80a557479efece6202e2ba2c9152061c83
vovilonn <vovanegrshopa9782@gmail.com>
2021-11-16 13:58:48 +0200

    commit

--- src/contract.js
+++ src/contract.js
@@ -1,7 +1,6 @@
 const { ethers } = require("ethers");
 const ABI = require("../etc/abi.json");
 const contractAdress = "0x4eF3c9b2AE34178c11aEC32Dc30bA87C9CF739e1";
-const myWallet = "0x0067aB17e1Dc650eC661685880B12331F57afA36";
 
 const provider = new ethers.providers.JsonRpcProvider(
     "https://eth-rinkeby.alchemyapi.io/v2/FeJ9BFIGGf3OGwRLZFIzUNyx-wPS3Rfb"
@@ -15,13 +14,5 @@ module.exports.getBirthday = async (id) => {
     } catch (err) {
         console.error(err);
         return false;
-    }
-};
-
-module.exports.tokensOfOwner = async (wallet) => {
-    try {
-        return await contractInstance.tokensOfOwner(wallet);
-    } catch (err) {
-        console.error(err);
     }
 };
--- src/etc.js
+++
@@ -1,8 +0,0 @@
-module.exports.checkDate = (birthday) => {
-    birthday = birthday.toNumber();
-    if (birthday === 0) {
-        return false;
-    }
-    const currentTime = Math.floor(Date.now() / 1000);
-    return currentTime > birthday + 518400; // current time > birthday + two weeks
-};
